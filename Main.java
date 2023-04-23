2227090a8c1147b49c8e777d5234b130a1956ae0
pat-em <122085350+pat-em@users.noreply.github.com>
2023-04-23 09:41:06 +0200

    FizzBuzz
    part two Java - podstawy w 2h

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+
+        for (; i < 101; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
